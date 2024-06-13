32bc98c24178d3ad4700bda7f54a6dfb164552f1
Erdi Yunus GÜL <erdiyunusfb@gmail.com>
2024-06-13 04:06:25 +0300

    Uptade

--- Assets/Scripts/Player1Move.cs
+++ Assets/Scripts/Player1Move.cs
@@ -12,6 +12,11 @@ public class Player1Move : MonoBehaviour
     private AnimatorStateInfo player1Layer0;
     private bool canWalkLeft = true;
     private bool canWalkRight = true;
+    public GameObject player1;
+    public GameObject opponent;
+    private Vector3 oppPosition;
+    private bool facingLeft = false;
+    private bool facingRight = true;
 
 
 
@@ -24,8 +29,11 @@ public class Player1Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Listen to the Animator
         player1Layer0 = anim.GetCurrentAnimatorStateInfo(0);
 
+
+        // Cannot exit screen
         Vector3 screenBounds = Camera.main.WorldToScreenPoint(this.transform.position);
 
         if (screenBounds.x > Screen.width - 200)
@@ -41,6 +49,20 @@ public class Player1Move : MonoBehaviour
             canWalkRight = true;
             canWalkLeft = true;
         }
+
+        // Get the opponet's position
+        oppPosition = opponent.transform.position;
+
+        //Facing left or right of the Opponent
+        if (oppPosition.x > player1.transform.position.x)
+        {
+            StartCoroutine(FaceLeft());
+        }
+        if (oppPosition.x < player1.transform.position.x)
+        {
+            StartCoroutine(FaceRight());
+        }
+
 
         // Walking left and right
         if (player1Layer0.IsTag("Motion"))
@@ -92,5 +114,27 @@ public class Player1Move : MonoBehaviour
     {
         yield return new WaitForSeconds(1.0f);
         ýsJumping = false;
+    }
+
+    IEnumerator FaceLeft()
+    {
+        if (facingLeft)
+        {
+            facingLeft = false;
+            facingRight = true;
+            yield return new WaitForSeconds(0.15f);
+            player1.transform.Rotate(0, -180, 0);
+        }
+    }
+
+    IEnumerator FaceRight()
+    {
+        if (facingRight)
+        {
+            facingLeft = true;
+            facingRight = false;
+            yield return new WaitForSeconds(0.15f);
+            player1.transform.Rotate(0, 180, 0);
+        }
     }
 }
