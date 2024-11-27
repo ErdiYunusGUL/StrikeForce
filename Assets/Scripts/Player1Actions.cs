9d995054c3cc286c61ca8d08a0810c79477b24fd
Erdi Yunus GÜL <erdiyunusfb@gmail.com>
2024-11-28 02:57:47 +0300

    Uptade

--- Assets/Scripts/Player1Actions.cs
+++ Assets/Scripts/Player1Actions.cs
@@ -7,20 +7,51 @@ public class Player1Actions : MonoBehaviour
 
     public float jumpSpeed = 1.0f;
     public GameObject player1;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("LightPunch");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            anim.SetTrigger("HeavyPunch");
+        }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            anim.SetTrigger("LightKick");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("HeavyKick");
+        }
     }
 
     public void JumpUp()
     {
         player1.transform.Translate(0, jumpSpeed, 0);
+    }
+
+    public void FlipUp()
+    {
+        player1.transform.Translate(0, jumpSpeed, 0);
+        player1.transform.Translate(0.1f, 0, 0);
+    }
+
+    public void FlipBack()
+    {
+        player1.transform.Translate(0, jumpSpeed, 0);
+        player1.transform.Translate(-0.1f, 0, 0);
     }
 }
